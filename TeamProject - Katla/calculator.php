685a511ee032cc38c8ff81b7eeabb07c3655255c
Gabriela Burlakova <gabriela.burlakova@gmail.com>
2014-07-03 01:27:37 +0300

    Add new styles to calculator.php

--- TeamProject - Katla/calculator.php
+++ TeamProject - Katla/calculator.php
@@ -24,26 +24,46 @@
 
 <body>
     <div id="header-wrapper"></div><!---Header Wrapper-->
-    <div class="push-main"></div>
+    <div class="push-content"></div>
+    <audio autoplay="autoplay" id="sound">
+        <source src="sounds/ше-пием.mp3" type="audio/mp3" id="music"/>
+    </audio>
+    <div class="buttons">
+        <img src="images/Play.png" alt="play" id="play"/>
+        <img src="images/Stop.png" alt="stop" id="stop"/>
+    </div>
     <main>
         <div class="container">
-            <div class="push-content"></div>
             <div class="row">
-                <div class="col-lg-12 col-md-12">
-                    <div class="well">
-        
-                        <h2>Калкулатор за изчисление на доза за приспиване след тежък ден</h2>
+                <div class="col-lg-8 col-lg-offset-2 col-md-12">
+                    <div class="well shadow">
+                        <h2>Изчисли своята лечебна доза</h2>
 						<form method="post" action="calculator.php">
-							<p><label  for="bug">Брой бъгове</label><input type="text" name="number1" id="bug"></p>
-							<p><label  for="hour">Отделено време</label><input type="text" name="number2" id="hour"></p>
-							<p><label  for="opt">Лекарство</label>
-								<select name="number3" id="opt">
-									<option value="2">Бира</option>
-									<option value="1">Ракия</option>
+                            <img src="images/no-water.jpg" alt="image" id="image" class="img-circle small-size"/>
+							<div>
+                                <label  for="bug" class="col-xs-8 col-xs-offset-2">Колко бъга имаше днес?</label>
+                                <input type="text" name="number1" id="bug">
+							</div>
+                            <div>
+                                <label  for="hour" class="col-xs-8 col-xs-offset-2">Колко часа коди</label>
+                                <input type="text" name="number2" id="hour">
+                            </div>
+							<div>
+                                <label  for="opt" class="margin-bottom">Избери си лекарство</label>
+							</div>
+
+								<select name="number3" id="opt" onchange="setImg(this)">
+                                    <option value=""></option>
+									<option value="2">
+                                        Бира
+                                    </option>
+									<option value="1">
+                                        Ракия
+                                    </option>
 								</select>
 							</p>
-							<input type="submit" value="relax">
-							</form>
+							<input type="submit" value="Judge">
+				        </form>
                     </div>
                 </div>
             </div><!-- /.row -->
@@ -85,6 +105,35 @@
         $(document).ready(function () {
             $('#header-wrapper').load('header-template.html');
         });
+    </script>
+    <script type="text/javascript">
+        function setImg(select){
+            var imageBox = document.getElementById("image");
+            var music = document.getElementById("music");
+            var sound = document.getElementById("sound");
+            if(select.value == "2"){
+                imageBox.removeAttribute("src");
+                imageBox.setAttribute("src", "images/beer-smile.jpg");
+                music.src = "sounds/бира.mp3";
+                sound.load();
+            } else{
+                imageBox.removeAttribute("src");
+                imageBox.setAttribute("src", "images/ракия.png");
+                music.src = "sounds/ракийчице.mp3";
+                sound.load();
+            }
+        }
+    </script>
+    <script type="text/javascript">
+        window.onload = function(){
+            var stop = document.getElementById('stop');
+            var play = document.getElementById('play');
+            var sound = document.getElementById('sound');
+            stop.addEventListener('click', function() {
+                sound.pause();}, false);
+            play.addEventListener('click', function(){
+                sound.play();}, false)
+        };
     </script>
 </body>
 
